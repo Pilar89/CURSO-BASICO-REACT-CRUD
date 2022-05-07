32dd3c43ddcdd038e82430d1034e8e30512d82c5
pilar <ingeniera.coral@gmail.com>
2022-05-06 23:42:20 -0500

    Estructura inicial -- se agrega estructura inical del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add User</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users} deleteUser={deleteUser} editRow={editRow} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
