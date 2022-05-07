6663fd9f00be292e5393f5393fba378cfe083e84
pilar <ingeniera.coral@gmail.com>
2022-05-07 00:46:42 -0500

    Componentes y App -- se crean componentes y demas elemetos vistos en el video

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,65 @@
+import React, { useState } from "react";
+import UserTable from "./components/UserTable";
+import AddUserForm from "./components/AddUserForm";
+import { v4 as uuidv4 } from "uuid";
+import EdiUserForm from "./components/EditUserForm";
+
 function App() {
+  // permite agregar los usuarios
+  const usersData = [
+    { id: uuidv4(), name: "Tania", username: "floppydiskette" },
+    { id: uuidv4(), name: "Craig", username: "siliconeidolon" },
+    { id: uuidv4(), name: "Ben", username: "benisphere" },
+  ];
+
+  const [users, setUsers] = useState(usersData);
+
+  const addUser = (user) => {
+    user.id = uuidv4();
+    setUsers([...users, user]);
+  };
+
+  // permite eliminar usuario
+  const deleteUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  //Permite editar usuario
+  const [editing, setEditing] = useState(false);
+
+  const initialFormState = { id: null, name: "", username: "" };
+  const [currentUser, setCurrentUser] = useState(initialFormState);
+
+  const editRow = (user) => {
+    setEditing(true);
+    setCurrentUser({ id: user.id, name: user.name, username: user.username });
+  };
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false);
+    setUsers(users.map((user) => (user.id === id ? updatedUser : user)));
+  };
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add User</h2>
+          {editing ? (
+            <div>
+              <h2>Edit user</h2>
+              <EdiUserForm
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </div>
+          )}
         </div>
         <div className="flex-large">
           <h2>View users</h2>
